13f3a7090acfb1d282b69cd2844a2119167ef1ee
Kyle <kyleslie2@gmail.com>
2020-04-25 14:51:20 -0400

    adding md transforms

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,13 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
     },
+    `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
