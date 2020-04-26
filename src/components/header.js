9eb25a057fdf1ae0b6096d75bc8e46998219c9c5
Kyle Leslie <31903847+kyleslie2@users.noreply.github.com>
2020-04-26 12:25:49 -0400

    working on header CSS

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,20 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import headerStyles from "./header.module.scss"
+
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `#639`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <header className={headerStyles.header}>
+    <div className={headerStyles.headerTitle}>
+      <h1>
+        <Link to="/" style={{color: `white`,textDecoration: `none`,}}>
           {siteTitle}
         </Link>
       </h1>
     </div>
-      <nav>
-        <ul>
+    <nav className={headerStyles.navContainer}>
+        <ul className={headerStyles.navList}>
           <li>
             <Link to="/">Home</Link>
           </li>
