2757e7c883c0549854fc7ccacbd4c9628b3c1415
Kyle Leslie <31903847+kyleslie2@users.noreply.github.com>
2020-07-20 10:31:01 -0400

    added shrug

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,9 +6,12 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+    <h1>404: NOT FOUND</h1>
+    <p>You just hit a route that doesn&#39;t exist... that sucks.</p>
+    <div style={{ fontSize: `90px`, marginTop: `10rem`, marginBottom: `1.45rem` }}>
+      <p>¯\_(ツ)_/¯</p>
+    </div> 
+    </Layout>
 )
 
 export default NotFoundPage
